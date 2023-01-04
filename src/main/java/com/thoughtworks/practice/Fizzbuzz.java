8365e78bd790343c6c690efc01b97d5b0e9ef35d
Fissehaye Girmay <fissehayegirmay@Fissehayes-MacBook-Pro-2.local>
2023-01-04 09:51:18 -0600

    Output [fizz] when an input number is divisible by 3

--- src/main/java/com/thoughtworks/practice/Fizzbuzz.java
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -1,4 +1,15 @@
 package com.thoughtworks.practice;
 
 public class Fizzbuzz {
+    public String handleFizzBuzz(String input) {
+        String output = null;
+        try {
+            if (Integer.parseInt(input) % 3 == 0) {
+                output = "fizz";
+            }
+        } catch (NumberFormatException nfx) {
+            System.out.println("Input is not a number");
+        }
+        return output;
+    }
 }
--- src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
+++ src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.practice;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FizzbuzzTest {
+public class FizzbuzzTest {
 
+    private Fizzbuzz subject;
+
+    @BeforeEach
+    public void setUp() {
+        subject = new Fizzbuzz();
+    }
+
+    @Test
+    public void handleFizzBuzz_GivenInputIsDivisibleBy3_Return_fizz() {
+        String input = "12";
+        String expectedOutput = "fizz";
+        assertEquals(expectedOutput, subject.handleFizzBuzz(input));
+    }
 }
